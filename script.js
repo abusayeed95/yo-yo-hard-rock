aa2b8d0b6212e511e6b9b9c6c60e9f387db97bc6
ahmedmusa995 <sayeedsayem8@gmail.com>
2020-08-17 18:23:07 +0600

    Oops

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
 function searchButtonWork() {
     const searchedSong = document.getElementById("searched-song").value;
-    fetch(`https://api.lyrics.ovh/suggest/${searchedSong}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            const arrays = data.data;
-            for (let i = 0; i <= 9; i++) {
-                const details = arrays[i];
-
-                const songTitle = details.title;
-                const songTitleSplit = songTitle.split(" ");
-                const songTitleURL = songTitleSplit.join("%20");
-
-                const albumTitle = details.album.title;
-
-                const artist = details.artist.name;
-                const artistSplit = artist.split(" ");
-                const artistURL = artistSplit.join("%20");
-
-                // fetch(`https://api.lyrics.ovh/v1/${artist}/${songTitle}`)
-                //     .then(response => response.json())
-                //     .then(data => data.lyrics)
-                //     .catch(error => console.log(error));
-                document.getElementById('results').innerHTML +=
-                    `<div class="single-result row align-items-center my-3 p-3">
-                        <div class="col-md-9">
-                            <h3 class="lyrics-name">${songTitle}</h3>
-                            <p class="author lead">Album by <span>${albumTitle}</span></p>
-                        </div>
-                        <div class="col-md-3 text-md-right text-center">
-                            <button class="btn btn-success" onclick="lyricsDropdown('lyrics${i}')">Get Lyrics</button>
-                        </div>
-                        <div class="align-items-center my-3 p-3 lyrics" id="lyrics${i}"> <pre>O laallaljdfkjkhfjkshfihj
-                        adfijsadhfioehfiosdg
-                        asdfhidshgfioedrfgjisd
-                        erfgtiudrguiodrtgiodj
-                        oetfgioerutiur/pre></div>
-                    </div>`
-            }
-        })
-        .catch(error => console.log(error))
+    if (searchedSong == "") {
+        alert('Please, write down first....');
+    }
+    else {
+        document.getElementById('results').style.display = 'block';
+        fetch(`https://api.lyrics.ovh/suggest/${searchedSong}`)
+            .then(response => response.json())
+            .then(data => {
+                // console.log(data);
+                const arrays = data.data;
+                for (let i = 0; i <= 9; i++) {
+                    const details = arrays[i];
+                    const songTitle = details.title;
+                    const artist = details.artist.name;
+                    const albumTitle = details.album.title;
+                    document.getElementById(`song-title-${i}`).innerHTML = `${songTitle}`;
+                    document.getElementById(`song-album-${i}`).innerHTML = `Album: ${albumTitle}`;
+                    document.getElementById(`artist-name-${i}`).innerHTML = `Artist: <span id="artist-${i}">${artist}</span>`;
+                }
+            })
+            .catch(error => console.log(error));
+    }
 }
 
-function lyricsDropdown(id) {
-    document.getElementById(id).style.display = "block";
+function getLyrics() {
+
+    for (let i = 0; i <= 9; i++) {
+        document.getElementById(`lyrics-area-${i}`).style.display = "block";
+        const artist = document.getElementById(`artist-${i}`).innerText;
+        const songTitle = document.getElementById(`song-title-${i}`).innerText;
+
+        fetch(`https://api.lyrics.ovh/v1/${artist}/${songTitle}`)
+            .then(res => res.json())
+            .then(data => {
+                const lyrics = data.lyrics;
+
+                document.getElementById(`lyrics-area-${i}`).innerHTML = `
+                <h2 class="text-success">Lyrics of ${songTitle}</h2>
+                <pre id="lyric-${i}">${lyrics}</pre>
+                <button class="btn btn-primary">Hide Lyrics</button>
+                `
+                if (document.getElementById(`lyric-${i}`).innerText == "undefined") {
+                    document.getElementById(`lyrics-area-${i}`).style.display = "none";
+                }
+
+            })
+    }
 }
